e085ccbc0347d2bd41567111ec7b8132a467a5bc
Lilii <lilii-ann.older@voco.ee>
2023-04-04 13:57:12 +0300

    ül 16 Harjutus 3 (kalkulaator2)

--- __tests__/komplekt6.ts
+++ __tests__/komplekt6.ts
@@ -23,3 +23,20 @@ test('simple input', ()=>{
     calcobj.pressButton('9');
     expect(calcobj.getPanelContents()).toBe("9");
 });
+test('multiple symbols input', ()=>{
+    calcobj.pressButton('7');
+    calcobj.pressButton('8');
+    expect(calcobj.getPanelContents()).toBe("78");
+});
+
+test('multiple symbols input', ()=>{
+    calcobj.pressButton('3');
+    calcobj.pressButton('2');
+    expect(calcobj.getPanelContents()).toBe("32");
+});
+test('multiple symbols input', ()=>{
+    calcobj.pressButton('1');
+    calcobj.pressButton('2');
+    calcobj.pressButton('3');
+    expect(calcobj.getPanelContents()).toBe("123");
+});
